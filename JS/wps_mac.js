b65c7dfe6c938ae0bdd018a7d30f3bac70dce032
饕餮 <86147928+taotie6@users.noreply.github.com>
2024-03-03 00:06:39 +0800

    Update wps_mac.js

--- JS/wps_mac.js
+++ JS/wps_mac.js
@@ -22,36 +22,79 @@ hostname = *account.wps.cn, *account.wps.com
 **************************/
 
 var body = JSON.parse($response.body);
+var expire_time_diy = 4102415999;
+var times = expire_time_diy - body.server_time;
 var obj = {
-  result: "ok",
-  server_time: body.server_time,
-  privilege: [
-    { spid: "data_recover", times: 0, expire_time: 4102415999 },
-    { spid: "ocr", times: 0, expire_time: 4102415999 },
-    { spid: "pdf2doc", times: 0, expire_time: 4102415999 },
-    { spid: "pdf_merge", times: 0, expire_time: 4102415999 },
-    { spid: "pdf_sign", times: 0, expire_time: 4102415999 },
-    { spid: "pdf_split", times: 0, expire_time: 4102415999 }
-  ],
-  total_cost: -30,
-  userid: body.userid,
-  level: 3,
-  wealth: 0,
-  exp: 0,
-  vip: {
-    memberid: 40,
-    expire_time: 4102415999,
-    name: "超级会员",
-    has_ad: 0,
-    enabled: [
-      { memberid: 40, name: "超级会员", expire_time: 4102415999 },
-      { memberid: 20, name: "WPS会员", expire_time: 4102415999 },
-      { memberid: 12, name: "稻壳会员", expire_time: 4102415999 }
-    ]
-  },
-  expire_time: 4102415999,
-  total_buy: 0,
+    "exp": 0,
+    "level": 1,
+    "privilege": [
+        {
+            "spid": "ocr",
+            "times": times,
+            "expire_time": expire_time_diy
+        },
+        {
+            "spid": "pdf2doc",
+            "times": times,
+            "expire_time": expire_time_diy
+        },
+        {
+            "spid": "pdf_merge",
+            "times": times,
+            "expire_time": expire_time_diy
+        },
+        {
+            "spid": "pdf_sign",
+            "times": times,
+            "expire_time": expire_time_diy
+        },
+        {
+            "spid": "pdf_split",
+            "times": times,
+            "expire_time": expire_time_diy
+        },
+        {
+            "spid": "speech_record",
+            "times": times,
+            "expire_time": expire_time_diy
+        },
+        {
+            "spid": "data_recover",
+            "times": times,
+            "expire_time": expire_time_diy
+        }
+    ],
+    "result": "ok",
+    "server_time": body.server_time,
+    "total_buy": 0,
+    "total_cost": 0,
+    "userid": body.userid,
+    "vip": {
+        "name": "超级会员",
+        "has_ad": 0,
+        "memberid": 40,
+        "expire_time": expire_time_diy,
+        "enabled": [
+            {
+                "memberid": 40,
+                "name": "超级会员",
+                "expire_time": expire_time_diy
+            },
+            {
+                "memberid": 20,
+                "name": "WPS会员",
+                "expire_time": expire_time_diy
+            },
+            {
+                "memberid": 12,
+                "name": "稻壳会员",
+                "expire_time": expire_time_diy
+            }
+        ]
+    },
+    "wealth": 0
 };
+
 
 $done({ body: JSON.stringify(obj) });
   
